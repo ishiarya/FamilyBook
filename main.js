77564485155b61e24c7e404b4c90fa5238a8041e
ishiarya <96100229+ishiarya@users.noreply.github.com>
2022-01-15 21:29:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","IMG_6580.jpg", "IMG_20201114_190305.jpg" , "IMG_20200215_212856.jpg", "IMG_202000415_114643.jpg" ];
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","IMG_202111104", "IMG_20211010.jpg" , "IMG_202111101.jpg", "IMG_20171019_211256.jpg"];
+           
 var names = ["Fmaily Book","Ishita Kharya", "Eshan Kharya", "Rachna Kharya", "Umesh Kharya"];
+          
 var i = 0;
 function update()
 {
-    i++;
-    var numbers_of_family_member_in_array = 4
-    if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
-      }
-    
-    //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [IMG_6580.jpg , IMG_20201114_190305.jpg , IMG_20200215_212856.jpg , IMG_20200415_114643.jpg]
-    //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [Ishita Kharya and Eshan Kharya , Eshan Kharya and Umesh Kharya , Eshan Kharya and Ishita Kharya , Rachna Kharya and Umesh Kharya] ;
- 
-    document.getElementById("family_member_image").src = updatedImage;
-    document.getElementById("family_member_name").innerHTML = updatedName;
-}
+     i++;
+     var numbers_of_family_member_in_array = 5
+     if(i > numbers_of_family_member_in_array )
+     {
+        i = 0;
+     }
+     var updatedImage = images[i];
+     var updatedName  = names[i];
+     document.getElementById("family_member_image").src = updatedImage;
+     document.getElementById("family_member_name").innerHTML = updatedName; 
